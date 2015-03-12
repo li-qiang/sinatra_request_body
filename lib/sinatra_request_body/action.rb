3b53655d670f2dc89b25f86b9994acda01371eec
li-qiang <li.q.q@qq.com>
2015-03-12 11:36:40 +0800

    liqiang - fix helper name

--- lib/sinatra_request_body/action.rb
+++ lib/sinatra_request_body/action.rb
@@ -3,7 +3,7 @@ module Sinatra
   module RequestBody
 
     def self.registered(app)
-      app.helpers SinatraRequestBody::Helpers
+      app.helpers SinatraRequestBody::Helper
       app.before do
         merge_to_params
         _symbolize(params)
